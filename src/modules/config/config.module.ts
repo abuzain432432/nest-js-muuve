d3f0fbbbff81dfd3da5ec56b6368ec01ccadc7bd
abuzain432432 <abuzain432432@gmail.com>
2024-10-24 22:56:08 +0500

    env configuration updated

--- src/modules/config/config.module.ts
+++ src/modules/config/config.module.ts
@@ -1,26 +1,33 @@
 import * as path from 'path';
+import * as fs from 'fs';
 import { Module, Global } from '@nestjs/common';
 import { ConfigModule as NextConfigModule } from '@nestjs/config';
-// const isDevEnvironment = process.env.NODE_ENV === 'development';
-// const isTestEnvironment = process.env.NODE_ENV === 'test';
 import { ConfigService } from './config.service';
 import envSchema from 'src/common/schemas/envs.schema';
+const envFilePath = () => {
+  switch (process.env.NODE_ENV) {
+    case 'development':
+      return path.resolve('./.env.local');
+    case 'production':
+      return path.resolve('./.env.prod');
+    default:
+      return path.resolve('./.env.test');
+  }
+};
+
+// Function to check if the environment file exists
+const envFileExists = (filePath: string): boolean => {
+  return fs.existsSync(filePath);
+};
 
 @Global()
 @Module({
   imports: [
     NextConfigModule.forRoot({
       cache: true,
-      expandVariables: true, //NOTE This is required to enable the variable extension in the environments variables file like this ${database}/xyz,
-      // envFilePath: path.resolve('./.env.test'),
-      envFilePath: path.resolve('./.env.local'),
-
-      // envFilePath: isDevEnvironment
-      //   ? path.resolve('./.env.local')
-      //   : isTestEnvironment
-      //     ? path.resolve('./.env.test')
-      //     : path.resolve('./.env.prod'),
+      expandVariables: true,
       validationSchema: envSchema,
+      envFilePath: envFileExists(envFilePath()) ? envFilePath() : undefined,
     }),
   ],
   providers: [ConfigService],
