2f5e4da49d5476300acc4076a0414c54241d0fe0
abuzain432432 <abuzain432432@gmail.com>
2024-10-27 21:07:52 +0500

    e2e test time increased

--- test-api/constants/index.ts
+++ test-api/constants/index.ts
@@ -1,5 +1,5 @@
 const baseApiUrl = 'http://localhost:3000/api';
-const FIFTEEN_SECONDS_IN_MS = 15000;
+const FIFTY_SECONDS_IN_MS = 50000;
 const mailosaurApiKey =
   process.env.MAILOSAUR_API_KEY || 'jtiX3i70xhtzq21c5WxQbeMQew6YTOnl';
 const mailosaurServerId = 'cgasbvwc';
@@ -7,7 +7,7 @@ const mailosaurServerId = 'cgasbvwc';
 const mailosaurEmail = 'cgasbvwc@mailosaur.net';
 export {
   baseApiUrl,
-  FIFTEEN_SECONDS_IN_MS,
+  FIFTY_SECONDS_IN_MS as FIFTEEN_SECONDS_IN_MS,
   mailosaurApiKey,
   mailosaurServerId,
   mailosaurEmail,
